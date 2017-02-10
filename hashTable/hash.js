371dda30bbc932188d07f1ec7261a26e8b3e8c30
a-faraz <anjumfaraz10@gmail.com>
2017-02-09 23:50:20 -0600

    Hash Table - complete

--- hashTable/hash.js
+++ hashTable/hash.js
@@ -1,6 +1,6 @@
 // Hash Table Implementation
 
-// the HashTable class
+// the HashTable constructor functions
 function HashTable(size) {
 	this.buckets = Array(size);
 	this.numBuckets = this.buckets.length;
@@ -21,19 +21,64 @@ HashTable.prototype.hash = function(key) {
 	}
 	var bucket = (total % this.numBuckets);
 	return bucket;
-	
+
 };
 
 // method which will add HashNodes to our class
 HashTable.prototype.insert = function(key, value) {
-	// body...
+
+	// hash the key
+	var index = this.hash(key);
+	// if there is no existing node in this bucket - create one
+	if (!this.buckets[index]) {
+		this.buckets[index] = new HashNode(key, value);
+		// if the first node has the same key - update the value
+	} else if (this.buckets[index].key === key) {
+		this.buckets[index].value = value;
+		// otherwise check next nodes until a match is found - then update
+	} else {
+		var currentNode = this.buckets[index];
+		while (currentNode.next) {
+			if (currentNode.next.key === key) {
+				currentNode.next.value = value;
+				return;
+			}
+			currentNode = currentNode.next;
+		}
+		// no match is found in next nodes - create new node
+		currentNode.next = new HashNode(key, value)
+	}
+
 };
 
 // method which will get HashNodes from our class
 HashTable.prototype.get = function(key) {
-	// body...
+	// hash the key to get the bucket
+	var index = this.hash(key);
+	// if bucket is empty - return null
+	if (!this.buckets[index]) {
+		return null;
+		// otherwise search bucket until matching key is found - then return its value
+	} else {
+		var currentNode = this.buckets[index];
+		while (currentNode) {
+			if (currentNode.key === key) {
+				return currentNode.value;
+			}
+			currentNode = currentNode.next;
+		}
+	}
 };
 
-HashTable.prototype.retrieveAll = function(first_argument) {
-	// body...
+// method which will return array of all hashNodes
+HashTable.prototype.retrieveAll = function() {
+	var allNodes = [];
+	for (var i = 0; i < this.numBuckets; i++) {
+		var currentNode = this.buckets[i];
+		while (currentNode) {
+			allNodes.push(currentNode);
+			currentNode = currentNode.next;
+		}
+	}
+	return allNodes;
 };
